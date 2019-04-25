0321cbe7e27fc8848c9a23876ea009c5c57d4726
karlapaulina <karlottaxgonzalez@gmail.com>
2019-04-25 13:56:46 -0400

    feat: added paddle.js and printed board and paddles to html

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,13 +1,64 @@
+import Board from './Board';
+import Paddle from './Paddle';
+import { SVG_NS, KEYS } from '../settings';
+
+
 export default class Game {
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
     this.height = height;
 
-		// Other code goes here...
+    this.gameElement = document.getElementById(this.element)
+
+    //Create instance of board
+    this.board = new Board(this.width, this.height);
+
+    //Dimensions for Player paddles instances
+    this.paddleWidth = 8;
+    this.paddleHeight = 56;
+    this.boardGap = 10;
+
+    //Player 1 instance
+    this.player1 = new Paddle(
+      this.height,
+      this.paddleWidth,
+      this.paddleHeight,
+      this.boardGap,
+      ((this.height - this.paddleHeight) / 2),
+      KEYS.a,
+      KEYS.z
+    )
+
+    //Player 2 instance
+    this.player2 = new Paddle(
+      this.height,
+      this.paddleWidth,
+      this.paddleHeight,
+      ((this.width - this.boardGap) - this.paddleWidth),
+      ((this.height - this.paddleHeight) / 2),
+      KEYS.up,
+      KEYS.down,
+    )
   }
 
+
   render() {
-		// More code goes here....
+    this.gameElement.innerHTML = '';
+
+
+    let svg = document.createElementNS(SVG_NS, 'svg');
+
+    svg.setAttributeNS(null, 'width', this.width);
+    svg.setAttributeNS(null, 'height', this.height);
+    svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
+    this.gameElement.appendChild(svg)
+
+    //Renders board by passing svg canvas we created
+    this.board.render(svg)
+
+    //Render P1 and P2 paddles
+    this.player1.render(svg);
+    this.player2.render(svg);
   }
 }
---
+++ src/partials/Paddle.js
@@ -0,0 +1,37 @@
+import { SVG_NS } from '../settings';
+
+export default class Paddle {
+    constructor(boardHeight, width, height, x, y, up, down) {
+        this.boardHeight = boardHeight;
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.speed = 10;
+        this.score = 0;
+
+        document.addEventListener('keydown', event => {
+            switch(event.key) {
+                case up:
+                this.y = (this.y - this.speed)
+                break;
+                case down:
+                this.y = (this.y + this.speed)
+                break;
+            }
+          });
+        
+    }
+
+
+    render(svg) {
+        let rect = document.createElementNS(SVG_NS, 'rect');
+        rect.setAttributeNS(null, 'fill', '#FFF');
+        rect.setAttributeNS(null, 'width', this.width);
+        rect.setAttributeNS(null, 'height', this.height);
+        rect.setAttributeNS(null, 'x', this.x);
+        rect.setAttributeNS(null, 'y', this.y);
+
+        svg.appendChild(rect);
+    }
+}
